21867a11e38440541475f32e4cc5f43a15e38d98
Danchi17 <atsushi17cyobi@yahoo.co.jp>
2024-09-01 20:51:08 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,6 +1,5 @@
 import { setupConnection, sendToAll, setupConnectionListener } from './network.js';
 import { updateUI } from './ui.js';
-import { roles } from './roles.js';
 
 let peer;
 export let gameState = {
@@ -21,6 +20,21 @@ export let currentPlayer = { id: "", name: "", role: "", originalRole: "", point
 export let isHost = false;
 
 const phases = ["待機中", "役職確認", "占い師", "人狼", "怪盗", "議論", "投票", "結果"];
+
+export const roles = [
+    { name: "占い師", team: "市民", cost: 3, ability: "占い師のターン、誰か一人の役職を確認する。もしくは場札の2枚の役職を確認する" },
+    { name: "占星術師", team: "市民", cost: 2, ability: "場札を含め、6枚の場にある役職の内、人狼陣営が何個あるか数が分かる。" },
+    { name: "ギャンブラー", team: "市民", cost: 1, ability: "日中のターン中にギャンブルボタンを押すと場札の役職のどちらか1つと入れ替えることが出来る。" },
+    { name: "無法者", team: "市民", cost: 1, ability: "投票完了後、敗北していた時にランダムに他の人と役職を交換する。勝利条件が入れ替わる" },
+    { name: "村長", team: "市民", cost: 3, ability: "他のプレイヤーと違い、投票数が2票となる。" },
+    { name: "怪盗", team: "市民", cost: 1, ability: "怪盗のターン時に他のプレイヤーと役職を入れ替えることが出来る" },
+    { name: "大熊", team: "人狼", cost: 5, ability: "自身が吊られた時、プレイヤーの過半数が人狼サイドなら強制勝利する(スパイは人狼陣営に含まない)" },
+    { name: "占い人狼", team: "人狼", cost: 4, ability: "誰か一人のカードを確認する。もしくは場のカードを2枚確認する。狼のターン時に他の人狼と仲間同士であることを確認できない" },
+    { name: "やっかいな豚男", team: "人狼", cost: 3, ability: "任意の他のプレイヤー1人に★マークを付与する。★マークは1分間で消滅する。" },
+    { name: "蛇女", team: "人狼", cost: 3, ability: "同数投票で処刑される場合、単独で特殊勝利する。他の人狼陣営のプレイヤーは敗北とする。" },
+    { name: "博識な子犬", team: "人狼", cost: 3, ability: "日中のターンに市民陣営の特定の役職（占星術師、ギャンブラー、無法者、村長）を当てることが出来れば、人狼陣営の勝利となる。" },
+    { name: "スパイ", team: "市民", cost: 2, ability: "狼のターン時に人狼陣営とお互いを確認できる。スパイ通報ボタンが設置され、バレると市民陣営が強制敗北となる。" }
+];
 
 export function initializeGame() {
     return new Promise((resolve, reject) => {
