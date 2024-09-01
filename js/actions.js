ee28b6895f5684d1db9ad61f4c8c92518dea4f43
Danchi17 <atsushi17cyobi@yahoo.co.jp>
2024-09-01 18:57:18 +0900

    Update actions.js

--- js/actions.js
+++ js/actions.js
@@ -1,8 +1,7 @@
-
 import { gameState, currentPlayer, updateGameState } from './game.js';
 import { sendToAll } from './network.js';
 import { updateUI } from './ui.js';
-import { roles } from './roles/roles.js';
+import { roles } from './roles.js';
 
 export function performAction(action, target) {
     if (gameState.actions[currentPlayer.id]) {
