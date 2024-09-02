775f74f968fcdd6db78d11242d4caf72e18309cd
Danchi17 <atsushi17cyobi@yahoo.co.jp>
2024-09-02 14:37:22 +0900

    Update gameSetup.js

--- js/game/gameSetup.js
+++ js/game/gameSetup.js
@@ -1,1 +1,30 @@
+import { roles } from './roleLogic.js';
+import { shuffleArray } from './gameUtils.js';
 
+export const createInitialState = () => ({
+    players: [],
+    phase: '待機中',
+    assignedRoles: {},
+    centerCards: [],
+    actions: {},
+    votes: {},
+    result: '',
+});
+
+export const initializeGame = () => {
+    const state = createInitialState();
+    return state;
+};
+
+export const setupRoles = (playerIds) => {
+    const shuffledRoles = shuffleArray([...roles]);
+    const playerRoles = shuffledRoles.slice(0, playerIds.length);
+    const centerCards = shuffledRoles.slice(playerIds.length, playerIds.length + 2);
+
+    const assignedRoles = {};
+    playerIds.forEach((id, index) => {
+        assignedRoles[id] = playerRoles[index].name;
+    });
+
+    return { assignedRoles, centerCards };
+};
