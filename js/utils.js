75cf6689b8bbbff83b46f9d9c4d84271d1713dfa
Danchi17 <atsushi17cyobi@yahoo.co.jp>
2024-09-02 14:39:12 +0900

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,12 @@
+// このファイルには、プロジェクト全体で使用される汎用的なユーティリティ関数を定義します。
+// 現時点では特定の関数は定義されていませんが、必要に応じて追加できます。
 
+export const formatDate = (date) => {
+    return new Date(date).toLocaleString();
+};
+
+export const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+// その他のユーティリティ関数をここに追加
