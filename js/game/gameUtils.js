b5ae771622e0fb4068fde891377e12e2c5033e1d
Danchi17 <atsushi17cyobi@yahoo.co.jp>
2024-09-02 20:12:22 +0900

    Update gameUtils.js

--- js/game/gameUtils.js
+++ js/game/gameUtils.js
@@ -1,4 +1,6 @@
-export const shuffleArray = (array) => {
+'use strict';
+
+window.shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
@@ -6,6 +8,6 @@ export const shuffleArray = (array) => {
     return array;
 };
 
-export const generateId = () => {
+window.generateId = () => {
     return Math.random().toString(36).substr(2, 9);
 };
