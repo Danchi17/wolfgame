73a6e9f29f9b12c974d3cf1f9b905668736861e7
Danchi17 <atsushi17cyobi@yahoo.co.jp>
2024-09-02 15:24:21 +0900

    Update gameState.js

--- js/game/gameState.js
+++ js/game/gameState.js
@@ -1,4 +1,14 @@
-import { createInitialState } from './gameSetup.js';
+// gameState.js
+
+const createInitialState = () => ({
+    players: [],
+    phase: '待機中',
+    assignedRoles: {},
+    centerCards: [],
+    actions: {},
+    votes: {},
+    result: '',
+});
 
 let gameState = createInitialState();
 
@@ -11,5 +21,46 @@ export const updateGameState = (newState) => {
 
 export const resetGameState = () => {
     gameState = createInitialState();
+    return gameState;
+};
+
+export const initializeGame = () => {
+    resetGameState();
+    return gameState;
+};
+
+// 他の必要な関数をここに追加
+export const addPlayer = (player) => {
+    gameState.players.push(player);
+    return gameState;
+};
+
+export const assignRoles = (roles) => {
+    gameState.assignedRoles = roles;
+    return gameState;
+};
+
+export const setCenterCards = (cards) => {
+    gameState.centerCards = cards;
+    return gameState;
+};
+
+export const setPhase = (phase) => {
+    gameState.phase = phase;
+    return gameState;
+};
+
+export const addAction = (playerId, action) => {
+    gameState.actions[playerId] = action;
+    return gameState;
+};
+
+export const addVote = (voterId, targetId) => {
+    gameState.votes[voterId] = targetId;
+    return gameState;
+};
+
+export const setResult = (result) => {
+    gameState.result = result;
     return gameState;
 };
