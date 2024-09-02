88581ee9926f84acc2c4d2bdb8f6f7889c3117c8
Danchi17 <atsushi17cyobi@yahoo.co.jp>
2024-09-02 14:36:48 +0900

    Update gameState.js

--- js/game/gameState.js
+++ js/game/gameState.js
@@ -1,1 +1,15 @@
+import { createInitialState } from './gameSetup.js';
 
+let gameState = createInitialState();
+
+export const getGameState = () => gameState;
+
+export const updateGameState = (newState) => {
+    gameState = { ...gameState, ...newState };
+    return gameState;
+};
+
+export const resetGameState = () => {
+    gameState = createInitialState();
+    return gameState;
+};
