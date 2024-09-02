3683a54a932d78f44cfc5ff1b3414569b3498645
Danchi17 <atsushi17cyobi@yahoo.co.jp>
2024-09-02 14:36:32 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,9 @@
-import { initializeGame, createGame, joinGame, startGame, nextPhase, resetGame } from './game.js';
-import { updateUI } from './ui.js';
+import { initializeGame } from './game/gameSetup.js';
+import { renderUI } from './ui.js';
+import { setupNetwork } from './network.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    initializeGame();
-    
-    const createGameButton = document.getElementById('createGameButton');
-    const joinGameButton = document.getElementById('joinGameButton');
-    const startGameButton = document.getElementById('startGame');
-    const nextPhaseButton = document.getElementById('nextPhase');
-    const resetGameButton = document.getElementById('resetGame');
-
-    if (createGameButton) createGameButton.addEventListener('click', createGame);
-    if (joinGameButton) joinGameButton.addEventListener('click', joinGame);
-    if (startGameButton) startGameButton.addEventListener('click', startGame);
-    if (nextPhaseButton) nextPhaseButton.addEventListener('click', nextPhase);
-    if (resetGameButton) resetGameButton.addEventListener('click', resetGame);
-
-    updateUI();
+    const gameState = initializeGame();
+    setupNetwork(gameState);
+    renderUI(gameState);
 });
