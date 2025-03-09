bae7a00c3bb03a6f98be380ae51e0f3358a1a32d
Danchi17 <atsushi17cyobi@yahoo.co.jp>
2025-03-09 19:26:26 +0900

    Update roles.js

--- js/roles.js
+++ js/roles.js
@@ -1,1 +1,95 @@
+// js/roles.js
+// 役職の定義
+const ROLES = {
+  // 市民陣営
+  SEER: {
+    name: '占い師',
+    team: 'village',
+    cost: 3,
+    description: '占い師のターン、誰か一人の役職を確認する。もしくは場札の2枚の役職を確認する'
+  },
+  FORTUNE_TELLER: {
+    name: '占星術師',
+    team: 'village',
+    cost: 2,
+    description: '場札を含め、6枚の場にある役職の内、人狼陣営が何個あるか数が分かる。'
+  },
+  APPRENTICE: {
+    name: '占い師の弟子',
+    team: 'village',
+    cost: 2,
+    description: '占い師のターン、誰か一人の役職を確認する。'
+  },
+  OUTLAW: {
+    name: '無法者',
+    team: 'village',
+    cost: 1,
+    description: '投票完了後、敗北していた時にランダムに他の人と役職を交換する。勝利条件が入れ替わる'
+  },
+  MAYOR: {
+    name: '村長',
+    team: 'village',
+    cost: 3,
+    description: '他のプレイヤーと違い、投票数が2票となる。'
+  },
+  THIEF: {
+    name: '怪盗',
+    team: 'village',
+    cost: 1,
+    description: '怪盗のターン時に他のプレイヤーと役職を入れ替えることが出来る'
+  },
+  SPY: {
+    name: 'スパイ',
+    team: 'village',
+    cost: 2,
+    description: '狼のターン時に人狼陣営とお互いを確認できる。スパイ通報ボタンが設置され、バレると市民陣営が強制敗北となる。'
+  },
+  
+  // 人狼陣営
+  WEREWOLF: {
+    name: '大熊',
+    team: 'werewolf',
+    cost: 5,
+    description: '自身が吊られた時、プレイヤーの過半数が人狼サイドなら強制勝利する（スパイは人狼陣営に含まない）'
+  },
+  SEER_WOLF: {
+    name: '占い人狼',
+    team: 'werewolf',
+    cost: 4,
+    description: '誰か一人のカードを確認する。もしくは場のカードを2枚確認する。狼のターン時に他の人狼と仲間同士であることを確認できない'
+  },
+  TROUBLESOME_PIG: {
+    name: 'やっかいな豚男',
+    team: 'werewolf',
+    cost: 3,
+    description: '任意の他のプレイヤー1人に★マークを付与する。★マークは1分間で消滅する。'
+  },
+  SNAKE_WOMAN: {
+    name: '蛇女',
+    team: 'werewolf',
+    cost: 3,
+    description: '同数投票で処刑される場合、単独で特殊勝利する。他の人狼陣営のプレイヤーは敗北とする。'
+  },
+  KNOWLEDGEABLE_PUPPY: {
+    name: '博識な子犬',
+    team: 'werewolf',
+    cost: 3,
+    description: '日中のターンに市民陣営の特定の役職を当てることが出来れば、人狼陣営の勝利となる。'
+  }
+};
 
+// 役職に関する機能
+function getRandomRoles(playerCount) {
+  // 利用可能な役職からランダムに選択
+  const allRoles = Object.values(ROLES);
+  const shuffled = [...allRoles].sort(() => 0.5 - Math.random());
+  
+  // プレイヤー数+場札2枚分の役職を選択
+  return shuffled.slice(0, playerCount + 2);
+}
+
+function isWerewolfTeam(role) {
+  return role && role.team === 'werewolf';
+}
+
+export { ROLES, getRandomRoles, isWerewolfTeam };
