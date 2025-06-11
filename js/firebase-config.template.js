09a091a21eaeba23328fef9d3541edcba117fba3
Danchi17 <atsushi17cyobi@yahoo.co.jp>
2025-06-11 20:48:14 +0900

    Firebase設定のテンプレートファイルを追加

---
+++ js/firebase-config.template.js
@@ -0,0 +1,14 @@
+// js/firebase-config.template.js
+// このファイルをコピーして firebase-config.js として保存し、
+// 実際のFirebase設定値を入力してください。
+
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
